bed3e9f23cb790fce8c7c2f8b2412516f2c7e7c0
Naimur <naeemhasan28@gmail.com>
2024-03-14 16:04:53 +0600

    added eslint and pretter config

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,17 +1,17 @@
-import { Inter } from 'next/font/google'
-import '@/styles/globals.scss'
 import ReduxProvider from '@/lib/redux/redux-provider'
+import '@/styles/globals.scss'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['200', '300', '400', '500', '700'] })
 
 export const metadata = {
-  title: 'NetWorth',
+  title: 'NetWorth'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className={inter.className} suppressHydrationWarning>
+      <body className={poppins.className} suppressHydrationWarning>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
